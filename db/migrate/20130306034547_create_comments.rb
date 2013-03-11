6c5403b9beff10d2aa59662d631f0e09760aaf69
Tsimeriani <clarissa.rodriguesmueller@gmail.com>
2013-03-11 16:27:40 +0100

    Update 20130306034547_create_comments.rb

--- db/migrate/20130306034547_create_comments.rb
+++ db/migrate/20130306034547_create_comments.rb
@@ -1,13 +1,1 @@
-class CreateComments < ActiveRecord::Migration
-  def change
-    create_table :comments do |t|
-      t.string :user_name
-      t.text :body
-      t.integer :obra_id
 
-      t.timestamps
-    end
-    add_foreign_key :comments, :obras
-
-  end
-end
